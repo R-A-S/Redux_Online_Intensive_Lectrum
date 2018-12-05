813124d5dcd4873d2c7fc278a267b86667c01483
RAS <r.a.s@live.ru>
2018-12-05 19:25:59 +0200

    homeWork lesson_4 (fail)

--- source/components/Posts/index.js
+++ source/components/Posts/index.js
@@ -13,7 +13,7 @@ import { mockedProfile } from '../../instruments/mockedData';
 import { Composer, Catcher, Post } from '../../components';
 
 // Actions
-import { fetchPostsAsync, createPostAsync } from '../../bus/posts/actions';
+import { postsActions } from '../../bus/posts/actions';
 
 const mapStateToProps = (state) => {
     return {
@@ -24,7 +24,10 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = (dispatch) => {
     return {
         actions: bindActionCreators(
-            { fetchPostsAsync, createPostAsync },
+            {
+                fetchPostsAsync: postsActions.fetchPostsAsync,
+                createPostAsync: postsActions.createPostAsync,
+            },
             dispatch
         ),
     };
